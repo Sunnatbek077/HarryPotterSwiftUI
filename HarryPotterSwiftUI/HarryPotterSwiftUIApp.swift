2b2d7b4510ef58457c686db2d5d3bc933968d121
Sunnatbek077 <sunnatbekabdunabiyev@icloud.com>
2025-08-31 15:31:16 +0500

    Initial Commit

---
+++ HarryPotterSwiftUI/HarryPotterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterSwiftUIApp.swift
+//  HarryPotterSwiftUI
+//
+//  Created by Sunnatbek on 31/08/25.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
